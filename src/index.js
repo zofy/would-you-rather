31ff54b7edd079eb9eeb9a15b905cd0b4f63404e
Patrik Puchala <patrik.puchala@ims.co.at>
2021-04-24 21:58:20 +0200

    Add Provider to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
