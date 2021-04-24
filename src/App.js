59f3c18c9b6832846e1408aeaac10995a93bbd63
Patrik Puchala <patrik.puchala@ims.co.at>
2021-04-24 21:57:52 +0200

    Add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import LoadingBar from 'react-redux-loading-bar'
 import './App.css';
+import AuthRoute from './components/AuthRoute'
+import LeaderBoard from './components/LeaderBoard'
+import Dashboard from './components/Dashboard'
+import Poll from './components/Poll'
+import NewPoll from './components/NewPoll'
+import Login from './components/Login'
+import Nav from './components/Nav'
+import NotFound from './components/NotFound';
+import { handleInitialData } from './actions/shared'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render () {
+    return (
+      <Router>
+          <Fragment>
+            <LoadingBar />
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/404' component={NotFound}/>
+              <Route>
+                <div className='container'>
+                  {this.props.loading === true
+                    ? null
+                    : (<div>
+                        <Nav />
+                        <Switch>
+                          <AuthRoute exact path='/' component={Dashboard} />
+                          <AuthRoute exact path='/new' component={NewPoll}/>
+                          <AuthRoute exact path='/leader' component={LeaderBoard} />
+                          <AuthRoute path='/poll/:id' component={Poll}/>
+                          <Redirect to='/404'/>
+                        </Switch>
+                    </div>)}
+                  </div>
+                </Route>
+              </Switch>
+            </Fragment>
+      </Router>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ loadingBar }) {
+  return {
+    loading: loadingBar.default === 1,
+  }
+}
+
+export default connect(mapStateToProps)(App);
